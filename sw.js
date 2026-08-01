6ae5ddbcc42afdcfdbce38d0d1e07ff39fdd3bf9
emillion-lab <emillion-lab@users.noreply.github.com>
2026-08-01 08:51:16 +0000

    Airport navigation buttons in place; v150/sw-v106

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // BakshishApp Service Worker
 // Кешира файловете за офлайн работа
 
-const CACHE_NAME = 'bakshishapp-bak-v105';
+const CACHE_NAME = 'bakshishapp-bak-v106';
 const STATIC_FILES = [
   '/BAK/',
   '/BAK/index.html',
